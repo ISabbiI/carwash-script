ba0e38e75c54adbc44c089bcf19d88a1a8fc70f2
Sabbi <93642978+ISabbiI@users.noreply.github.com>
2025-01-12 22:16:46 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,14 @@
+Config = {}
+
+-- Prix du lavage
+Config.WashPrice = 50
+
+-- Point de lavage
+Config.WashPoint = {
+    x = 50.912,
+    y = -1391.977,
+    z = 28.25
+}
+
+-- Distance d'interaction
+Config.InteractionDistance = 2.0 
